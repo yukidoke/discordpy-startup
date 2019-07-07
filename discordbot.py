bfcc549d7d58f8c10cdcb798b03f7b1bf0947079
yukidoke <50089293+yukidoke@users.noreply.github.com>
2019-07-07 22:01:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -34,7 +34,16 @@ async def r(ctx):
         await ctx.send(f'{a} + {b} = {c} CRITICAL!!')
     else:
         await ctx.send(f'{a} + {b} = {c}')
-        
+
+#slapper
+class Slapper(commands.Converter):
+    async def convert(self, ctx, argument):
+        to_slap = random.choice(ctx.guild.members)
+        return '{0.author} slapped {1} because *{2}*'.format(ctx, to_slap, argument)
+
+@bot.command()
+async def slap(ctx, *, reason: Slapper):
+    await ctx.send(reason)
 
 
 bot.run(token)
