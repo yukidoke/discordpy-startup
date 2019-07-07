6221a367368f6d0386b2f46e2aad17316c3acea0
yukidoke <50089293+yukidoke@users.noreply.github.com>
2019-07-07 22:16:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -35,5 +35,10 @@ async def r(ctx):
     else:
         await ctx.send(f'{a} + {b} = {c}')
 
+@bot.command()
+async def atk(ctx):
+    attack = random.choice(ctx.guild.members)
+    await ctx.send(f'The monster attacked {attack}.')
+
 
 bot.run(token)
