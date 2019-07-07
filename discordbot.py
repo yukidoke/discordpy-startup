109b76208a38edcffbdebdeec02c285f69004627
yukidoke <50089293+yukidoke@users.noreply.github.com>
2019-07-07 20:57:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
+import random
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -12,8 +13,11 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def r(ctx):
+    a = random.randint(1,6)
+    b = random.randint(1,6)
+    c = a + b
+    await ctx.send(f'{a} + {b} = {c}')
 
 
 bot.run(token)
