67203710523a8e3ea2831bcda4a23c76474f11ab
yukidoke <50089293+yukidoke@users.noreply.github.com>
2019-07-20 01:19:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,7 +15,7 @@ async def on_message(ctx):
 
 @bot.command()
 async def playing(ctx):
-await ctx.send(f'{ctx.author.display_name}')
+    await ctx.send(f'{ctx.author.display_name}')
 
 #概要を説明するコマンド
 @bot.command()
