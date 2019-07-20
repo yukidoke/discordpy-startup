ac72c84a7571714c6aada1dfd823c3638abe7a43
yukidoke <50089293+yukidoke@users.noreply.github.com>
2019-07-20 12:43:21 +0900

    Create save.py

---
+++ cogs/save.py
@@ -0,0 +1,28 @@
+from discord.ext import commands
+import shelve
+
+# コグとして用いるクラスを定義。
+class Save(commands.Cog):
+
+    # TestCogクラスのコンストラクタ。Botを受取り、インスタンス変数として保持。
+    def __init__(self, bot):
+        self.bot = bot
+
+    # コマンドの作成。コマンドはcommandデコレータで必ず修飾する。
+    @commands.command()
+    async def save(self, ctx):
+        character_sheet = []
+        for m in ctx.guild.members:
+            try:
+                chara = shelve.open('character.db')
+                dict = chara[str(ctx.m.id)]
+                character_sheet.append(dict)
+            except Exception:
+                pass
+        chara.close()
+        channel = client.get_channel(601095696082534410)
+        await channel.send(f'{character_sheet}')
+
+# Bot本体側からコグを読み込む際に呼び出される関数。
+def setup(bot):
+    bot.add_cog(Save(bot))
