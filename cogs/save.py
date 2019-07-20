cba5de5819d011d3624b5061fdaea4ee5d0ba3e1
yukidoke <50089293+yukidoke@users.noreply.github.com>
2019-07-20 13:29:40 +0900

    Update save.py

--- cogs/save.py
+++ cogs/save.py
@@ -9,16 +9,19 @@ class Save(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # コマンドの作成。コマンドはcommandデコレータで必ず修飾する。
+
+
     @commands.Cog.listener()
     async def on_message(self, ctx):
         if ctx.author.bot:
             return
         if ctx.content == 'save':
-        character_sheet = []
-        client = discord.Client()
-        channel = client.get_channel(601095696082534410)
-        await channel.send(f'{ctx.guild.members}')
+            character_sheet = []
+            client = discord.Client()
+            channel = client.get_channel(601095696082534410)
+            guild = discord.Guild()
+            member = discord.Member()
+            await channel.send(f'{guild.members}')
 
 # Bot本体側からコグを読み込む際に呼び出される関数。
 def setup(bot):
